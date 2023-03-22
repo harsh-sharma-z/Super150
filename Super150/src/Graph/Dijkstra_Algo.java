16f42448a6d7d34c0781a0afb50cd7c96ae1f5be
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-03-22 12:18:14 +0530

    College Code

---
+++ Super150/src/Graph/Dijkstra_Algo.java
@@ -0,0 +1,87 @@
+package Graph;
+
+import java.util.*;
+
+public class Dijkstra_Algo {
+    private static  HashMap<Integer, HashMap<Integer,Integer>> map=new HashMap<>();
+    public Dijkstra_Algo(int v)
+    {
+        for(int i=1;i<=v;i++)
+        {
+            map.put(i,new HashMap<>());
+        }
+    }
+
+    public static void addEdge(int v1,int v2,int cost)
+    {
+        map.get(v1).put(v2,cost); //v1-->v2
+        map.get(v2).put(v1,cost); //v2-->v1
+    }
+
+    private static class DijsktraPair {
+        int vtx;
+        String acq_path;
+        int cost;
+
+        public DijsktraPair(int vtx, String acq_path , int cost)
+        {
+            this.vtx=vtx;
+            this.acq_path=acq_path;
+            this.cost=cost;
+        }
+
+        @Override
+        public String toString()
+        {
+            return this.vtx+" val:"+this.acq_path+" @ "+this.cost;
+        }
+
+        public static void cost()
+        {
+            PriorityQueue<DijsktraPair> pq=new PriorityQueue<>(
+                    new Comparator<DijsktraPair>() {
+                        @Override
+                        public int compare(DijsktraPair o1, DijsktraPair o2) {
+                            return o1.cost-o2.cost;
+                        }
+                    });
+
+
+                    Set<Integer> vis=new HashSet<>();
+                    pq.add(new DijsktraPair(1,"1",0));
+                    while(!pq.isEmpty())
+                    {
+                        DijsktraPair rp=pq.remove();
+
+                        if(vis.contains(rp.vtx))
+                            continue;
+
+                        vis.add(rp.vtx);
+
+                        System.out.println(rp);
+
+                        for(int nbrs: map.get(rp.vtx).keySet())
+                            if(!vis.contains(nbrs))
+                            {
+                                int cost= rp.cost+ map.get(rp.vtx).get(nbrs);
+                                pq.add(new DijsktraPair(nbrs,rp.acq_path + nbrs, cost));
+                            }
+                    }
+
+        }
+
+        public static void main(String[] args) {
+            Dijkstra_Algo g=new Dijkstra_Algo(7);
+            g.addEdge(1,4,6);
+            g.addEdge(1,2,10);
+            g.addEdge(2,3,7);
+            g.addEdge(3,4,5);
+            g.addEdge(4,5,1);
+            g.addEdge(5,6,4);
+            g.addEdge(7,5,2);
+            g.addEdge(6,7,3);
+
+            cost();
+        }
+    }
+}
--- Super150/src/Main.java
+++ Super150/src/Main.java
@@ -4,45 +4,8 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc=new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-->0) {
 
-            int ULx = sc.nextInt();
-            int ULy = sc.nextInt();
-            int LRx = sc.nextInt();
-            int LRy = sc.nextInt();
-
-
-            int Dx = sc.nextInt();
-            int Dy = sc.nextInt();
-
-            int N = sc.nextInt();
-            int[][] trees = new int[N][2];
-            double[] radius = new double[N];
-            for (int i = 0; i < N; i++) {
-                trees[i][0] = sc.nextInt();
-                trees[i][1] = sc.nextInt();
-                radius[i] = sc.nextDouble();
-            }
-
-            int count = 0;
-            for (int R = 1; R <= Math.max(Dx - ULx, LRx - Dx); R++) {
-                boolean valid = true;
-                for (int i = 0; i < N; i++) {
-                    int x = trees[i][0];
-                    int y = trees[i][1];
-                    double d = Math.sqrt((x - Dx) * (x - Dx) + (y - Dy) * (y - Dy));
-                    if (d - radius[i] < R - 1e-9) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if (valid) {
-                    count++;
-                }
-            }
-
-            System.out.println("Case " + t + ": " + count);
-        }
+        for(int i=0;i<10000;i++)
+            System.out.println("DumDum!!!");
     }
 }
