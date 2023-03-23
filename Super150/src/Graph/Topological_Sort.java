520cb526b81167db9d4fe0ac3516de0a58b1bd17
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-03-23 09:20:04 +0530

    College Code

---
+++ Super150/src/Graph/Topological_Sort.java
@@ -0,0 +1,49 @@
+package Graph;
+import javax.swing.*;
+import java.util.*;
+public class Topological_Sort {
+    private HashMap<Integer,HashMap<Integer,Integer>> map=new HashMap<>();
+
+    public Topological_Sort(int v)
+    {
+        for(int i=1;i<=v;i++)
+            map.put(i,new HashMap<>());
+    }
+
+    public void addEdge ( int v1, int v2, int cost)
+    {
+        map.get(v1).put(v2,cost);
+    }
+
+    public int[] indegree(){
+        int[] in=new int[map.size()+1];
+        for(int key:map.keySet())
+        {
+            for(int nbrs:map.get(key).keySet())
+                in[nbrs]++;
+        }
+        return in;
+    }
+    public void TopoSort()
+    {
+        int in[]=indegree();
+        Queue<Integer> q=new LinkedList<>();
+
+        for(int i=1;i<in.length;i++)
+            if(in[i]==0)
+                q.add(i);
+
+        while(!q.isEmpty())
+        {
+            int key=q.peek();
+            q.remove();
+            System.out.print(key+" ");
+            for(int nbrs: map.get(key).keySet())
+            {
+                in[nbrs]--;
+                if(in[nbrs]==0)
+                    q.add(nbrs);
+            }
+        }
+    }
+}
---
+++ Super150/src/Graph/Topological_Sort_Client.java
@@ -0,0 +1,21 @@
+package Graph;
+
+import java.util.Arrays;
+
+public class Topological_Sort_Client {
+    public static void main(String[] args) {
+        Topological_Sort ts = new Topological_Sort(7);
+        ts.addEdge(1, 2,0);
+        ts.addEdge(1, 4,0);
+        ts.addEdge(4, 3,0);
+        ts.addEdge(3, 2,0);
+        ts.addEdge(4, 5,0);
+        ts.addEdge(5, 7,0);
+        ts.addEdge(6, 3,0);
+        ts.addEdge(6, 7,0);
+
+        System.out.println(Arrays.toString(ts.indegree())); //[0, 0, 2, 2, 1, 1, 0, 2]
+        //ts.addedge(3, 1,0);
+        ts.TopoSort();  //1 6 4 3 5 2 7
+    }
+}
