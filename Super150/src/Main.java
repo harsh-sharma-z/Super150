d523b92619fa269719662c469c71138542968156
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-04-05 22:16:33 +0530

    College Code

--- Super150/src/Main.java
+++ Super150/src/Main.java
@@ -1,54 +1,44 @@
-import java.lang.reflect.Array;
 import java.util.*;
-public class Main {
-    public static void main(String args[]) {
+class Main {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-->0){
-            int n=sc.nextInt();
-            int m=sc.nextInt();
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int[][] grid=new int[n][m];
         for(int i=0;i<n;i++)
-            adj.add(new ArrayList<>());
-        for(int i=0;i<m;i++)
         {
-            int u=sc.nextInt();
-            int v=sc.nextInt();
-            u--;
-            v--;
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-        if(a<=b)
-        {
-            System.out.println(a*n);
-        }
-        else {
-            int ans=0;
-            int[] vis=new int[n];
-            for(int i=0;i<n;i++ )
+            for(int j=0;j<m;j++)
             {
-                if(vis[i]==0)
-                {
-                    int k=dfs(i,adj,vis,0);
-                    ans=ans+((k-1)*b)+a;
+                grid[i][j]=sc.nextInt();
+            }
+        }
+        int area=0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[i].length; j++){
+                if(grid[i][j]==1){
+                    int sum=0;
+                    sum=island(grid,i,j,sum,n,m);
+                    area=Math.max(sum,area);
+                    }
                 }
             }
-            System.out.println(ans);
+        System.out.println(area);
         }
 
+
+
+    public static int island(int[][] grid, int i, int j, int count, int n, int m){
+        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]==0 ){
+            return count;
         }
+        grid[i][j]=0;
+        count=island(grid, i+1, j, count+1,n,m);
+        count=island(grid, i-1, j, count+1,n,m);
+        count=island(grid, i, j+1, count+1,n,m);
+        count=island(grid, i, j-1, count+1,n,m);
+        return count;
     }
-    private static int dfs(int node , ArrayList<ArrayList<Integer>> adj, int[] vis, int road)
-    {
-        vis[node]=1;
-        for(Integer i:adj.get(node))
-        {
-            if(vis[i]==0)
-                road+=dfs(i,adj,vis,road+1);
-        }
-        return road;
-    }
+
 }
+
+
