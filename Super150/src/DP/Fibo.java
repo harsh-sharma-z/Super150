3f04c5797647c58dca630cb700321ba5f37c7de7
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-04-05 10:03:57 +0530

    College Code

--- Super150/src/DP/Fibo.java
+++ Super150/src/DP/Fibo.java
@@ -2,14 +2,19 @@ package DP;
 import java.util.*;
 public class Fibo {
     public static void main(String[] args) {
-        int n=150;
+        int n=1000;
         long[] dp=new long[n+1];
         long a=(long)System.currentTimeMillis();
-        System.out.println(fib(n,dp));
+        System.out.println(fibTD(n,dp));
         long b=(long)System.currentTimeMillis();
         System.out.println(b-a);
+
+        a=(long)System.currentTimeMillis();
+        System.out.println(fibBU(n));
+        b=(long)System.currentTimeMillis();
+        System.out.println(b-a);
     }
-    public static long fib(int n,long[] dp)
+    public static long fibTD(int n,long[] dp)
     {
         if(n==0 || n==1)
         {
@@ -17,8 +22,22 @@ public class Fibo {
         }
         if(dp[n]!=0)
             return dp[n];
-        long f1=fib(n-1,dp);
-        long f2=fib(n-2,dp);
+        long f1=fibTD(n-1,dp);
+        long f2=fibTD(n-2,dp);
         return dp[n]=f1+f2;
+    }
+    public static long fibBU(int n)
+    {
+        long dp[]=new long[n+1];
+
+        dp[0]=0;
+        dp[1]=1;
+
+        for(int i=2;i<dp.length;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
 }
