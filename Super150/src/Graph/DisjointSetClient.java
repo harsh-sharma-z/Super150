a9722de139db49e65435ce0f3a585311a8b32240
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-04-09 17:52:28 +0530

    College Code

---
+++ Super150/src/Graph/DisjointSetClient.java
@@ -0,0 +1,65 @@
+package Graph;
+import java.util.*;
+class DisjointSet{
+    List<Integer> parent=new ArrayList<>();
+    List<Integer> rank=new ArrayList<>();
+    public DisjointSet(int n)
+    {
+        for(int i=0;i<=n;i++)
+        {
+            rank.add(0);
+            parent.add(i); // sab apne parent
+        }
+    }
+
+    public int findPar(int node)
+    {
+        if(node==parent.get(node)) //agar vo khudka parent hai
+            return node;
+        int ultpar=findPar(parent.get(node)); //recursively ultimate boss find karo
+        parent.set(node,ultpar); // lage haath parent update kardo rast evalo ke bhi
+        return parent.get(node); // return kardo updated list ki parent value
+    }
+
+    public void unionByRank(int u , int v)
+    {
+        int ult_u=findPar(u);
+        int ult_v=findPar(v);
+        if(ult_v==ult_u) // agar dono ke papa same hai to kuch nhi karne ka
+            return ;
+        if(rank.get(ult_u)<rank.get(ult_v)) // u chhota h v se attach
+        {
+            parent.set(ult_u,ult_v);
+        }
+        else if(rank.get(ult_v)<rank.get(ult_u)) // v chhota hai to u se attach
+        {
+            parent.set(ult_v,ult_u);
+        }
+        else {  // varna koi bhi kisi se bhi attach but rank badhana padega
+            parent.set(ult_u,ult_v);
+            int rankU=rank.get(ult_u);
+            rank.set(ult_u,rankU+1);
+        }
+    }
+}
+public class DisjointSetClient {
+    public static void main(String[] args) {
+        DisjointSet ds=new DisjointSet(7);
+        ds.unionByRank(1,2);
+        ds.unionByRank(2,3);
+        ds.unionByRank(4,5);
+        ds.unionByRank(6,7);
+        ds.unionByRank(5,6);
+        if(ds.findPar(3)==ds.findPar(7))
+            System.out.println("Same parent of 3 and 7");
+        else
+            System.out.println("Not same parent of 3 and 7");
+
+        //chalo inko jodte hai
+        ds.unionByRank(3,7);
+        if(ds.findPar(3)==ds.findPar(7))
+            System.out.println("Same parent of 3 and 7");
+        else
+            System.out.println("Not same parent of 3 and 7");
+    }
+}
